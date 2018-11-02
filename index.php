80ac347158d6f2c3f9ca18467cd0dc778df26426
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-02 11:07:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,23 +8,12 @@ if ($method == 'POST') {
     $requestBody = file_get_contents('php://input');
     $json        = json_decode($requestBody);
     
-    sleep(5);
-    
     $speech = "Primer mensaje";
     
     $response              = new \stdClass();
     $response->speech      = $speech;
     $response->displayText = $speech;
     $response->source      = "webhook";
-    
-    echo json_encode($response);
-    
-    sleep(5);
-    
-    $speech2 = "Segundo mensaje";
-    
-    $response->speech      = $speech2;
-    $response->displayText = $speech2;
     
     echo json_encode($response);
     
