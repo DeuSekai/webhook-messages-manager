4bce59eae895206946b0676cb1498dbb0d2a78cd
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-02 11:34:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
-if ($method == 'GET') {
+if ($method == 'POST') {
     
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
