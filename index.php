b202231f3277fb09c9177bf80b12a27e34c8cea8
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-02 10:27:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if ($method == 'POST') {
+    
     $requestBody = file_get_contents('php://input');
     $json        = json_decode($requestBody);
     
-    sleep(20);
+    sleep(5);
     
-    $speech = "Hi, Nice to meet you";
-    $speech2 = "Hi, Nice to meet you";
+    $speech = "Primer mensaje";
+    $speech2 = "Segundo mensaje";
     
     $response              = new \stdClass();
     $response->speech      = $speech;
     $response->displayText = $speech;
     $response->source      = "webhook";
+    
     echo json_encode($speech);
     
     sleep(20);
@@ -23,7 +25,9 @@ if ($method == 'POST') {
     echo json_encode($speech2);
     
 } else {
+    
     echo "Method not allowed";
+
 }
 
 ?> 
