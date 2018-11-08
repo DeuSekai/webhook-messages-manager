b6e7611f9a563e898825cd0a2fad46cfbf0b3054
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-08 13:05:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ if ($method == 'POST') {
             //$day = 60 * 60 * 24 + time(); // en un día
             //$month = 60 * 60 * 24 * 30 + time(); // en un mes
             //$year = 60 * 60 * 24 * 365 + time(); // en un año
+            setcookie("cookie");
             setcookie("cookie", $name/*,$month*/);
             $speech = NULL;
             break;
