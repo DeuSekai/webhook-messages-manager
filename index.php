6d221f6674144a78d8c260c7b7dbca3917dd078f
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-08 10:44:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <?php
-
+ 
 $method = $_SERVER['REQUEST_METHOD'];
-
+ 
 // Process only when method is POST
 if ($method == 'POST') {
-    
+   
     $name = "Amigo";
-    
+   
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
-    
+   
     $intent = $json->result->metadata->intentName;
    
     switch ($intent) {
-        
-        case 'Préstamo': 
-            global $name = $json->result->parameters->any;
-			$speech = $name;
-			break;
-
-		default:
-			$speech = $name;
-			break;
-            
-	}
-    
+	      
+	case 'Préstamo':
+            $name = $json->result->parameters->any;
+            $speech = $name;
+            break;
+ 
+        default:
+            $speech = $name;
+            break;
+           
+    }
+   
     $response = new \stdClass();
     $response->speech= $speech;
     $response->displayText = $speech;
     $response->source = "webhook";
-    
+   
     sleep(2);
-    
+   
     echo json_encode($response);
-    
+   
 } else {
-    
+   
     echo "Method not allowed";
-
+ 
 }
-
+ 
 ?> 
