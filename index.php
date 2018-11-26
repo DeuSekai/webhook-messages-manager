2416ee1595f6e8a82464cbbed50f43638f86e56c
DeuSekai <39871892+DeuSekai@users.noreply.github.com>
2018-11-26 10:56:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,27 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if ($method == 'POST') {
-  
+    
+    $requestBody = file_get_contents('php://input');
+    $json        = json_decode($requestBody);
+    
+    $text = $json->result->metadata->intentName;
+    
+    switch ($text) {
+        
+        case 'Default Welcome Intent':
+            $speech = "¡Feliz Viernes!\n\nDeja te platico sobre nuestros préstamos :D\n\nNuestros préstamos son para jubilados y pensionados, trabajadores del sector salud, de la educación y del gobierno.\n\nNuestros requisitos son:\n\n1.- Identificación oficial\n2.- Comprobante de domicilio\n3.- Comprobante de ingresos\n\n¡En Préstamo Feliz no necesitas aval además de que no checamos buró!\n\nTe recordamos que nuestros tramites son totalmente gratuitos.";
+            break;
+        
+        default:
+            $speech = NULL;
+            break;         
+        
+    }
+    
     $response              = new \stdClass();
-    $response->speech      = NULL;
-    $response->displayText = NULL;
+    $response->speech      = $speech;
+    $response->displayText = $speech;
     $response->source      = "webhook";
     
     //4 seconds delay
@@ -21,4 +38,4 @@ if ($method == 'POST') {
     
 }
 
-?> 
+?>
