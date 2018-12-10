5d7ae17517a980172f3e608b0aeff329d8436b76
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-10 12:34:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -64,13 +64,11 @@ $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["queryResult"]["action"])) {
     processMessage($update);
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-   fwrite($myfile, $update["queryResult"]["action"]);
-    fclose($myfile);
+    
 }else{
      sendMessage(array(
             "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
+            "fulfillmentText"=>NULL,
             "payload" => array(
                 "items"=>[
                     array(
