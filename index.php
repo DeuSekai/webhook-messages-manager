9673db6c267bc7dbd2d119d8c2956749c2beddfd
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-10 12:18:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,58 @@
 <?php
-
-
- if($update["result"]["action"] == "sayHello"){
+function processMessage($update) {
+    if($update["queryResult"]["action"] == "sayHello"){
         sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Hello from webhook",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"response from host"
+                         )
+                    )
+                ],
+                ),
+           
         ));
+    }else if($update["queryResult"]["action"] == "convert"){
+        if($update["queryResult"]["parameters"]["outputcurrency"] == "USD"){
+           $amount =  intval($update["queryResult"]["parameters"]["amountToConverte"]["amount"]);
+           $convertresult = $amount * 360;
+        }
+         sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"The conversion result is".$convertresult,
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"The conversion result is".$convertresult
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+    }else{
+        sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Error",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+        
     }
 }
  
@@ -17,8 +62,27 @@ function sendMessage($parameters) {
  
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
+if (isset($update["queryResult"]["action"])) {
     processMessage($update);
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+   fwrite($myfile, $update["queryResult"]["action"]);
+    fclose($myfile);
+}else{
+     sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Hello from webhook",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
 }
 
 
