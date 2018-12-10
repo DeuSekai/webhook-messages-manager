dd676d0334ea16c901175306a12412e37662773f
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-10 12:30:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
-function processMessage($update) {
-    if($update["queryResult"]["action"] == "sayHello"){
+
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if ($method == 'POST') {
+    
+    $update_response = file_get_contents("php://input");
+    $update          = json_decode($update_response, true);
+    
+    if ($update["queryResult"]["action"] == "sayHello") {
+        $week = array(
+            "Domingo",
+            "Lunes",
+            "Martes",
+            "Miercoles",
+            "Jueves",
+            "Viernes",
+            "Sábado"
+        );
+        $day  = $week[date('w')];
+        //1 seconds delay
+        sleep(1);
+        
         sendMessage(array(
             "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
+            "fulfillmentText"=>"Hola, ¡feliz {$day}!, Podrías compartirnos tu nombre completo y de qué estado de la república nos contactas para brindarte un mejor servicio. 𝗧𝗲 𝗿𝗲𝗰𝗼𝗿𝗱𝗮𝗺𝗼𝘀 𝗾𝘂𝗲 𝗻𝘂𝗲𝘀𝘁𝗿𝗼𝘀 𝘁𝗿𝗮𝗺𝗶𝘁𝗲𝘀 𝘀𝗼𝗻 𝘁𝗼𝘁𝗮𝗹𝗺𝗲𝗻𝘁𝗲 𝗴𝗿𝗮𝘁𝘂𝗶𝘁𝗼𝘀 😊",
             "payload" => array(
                 "items"=>[
                     array(
@@ -16,74 +41,41 @@ function processMessage($update) {
                 ),
            
         ));
-    }else if($update["queryResult"]["action"] == "convert"){
-        if($update["queryResult"]["parameters"]["outputcurrency"] == "USD"){
-           $amount =  intval($update["queryResult"]["parameters"]["amountToConverte"]["amount"]);
-           $convertresult = $amount * 360;
-        }
-         sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"The conversion result is".$convertresult,
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"The conversion result is".$convertresult
-                         )
-                    )
-                ],
-                ),
-           
-        ));
-    }else{
+        
+        
+    } else {
+        //1 seconds delay
+        sleep(4);
+
+
         sendMessage(array(
             "source" => $update["responseId"],
-            "fulfillmentText"=>"Error",
+            "fulfillmentText"=>NULL,
             "payload" => array(
                 "items"=>[
                     array(
                         "simpleResponse"=>
                     array(
-                        "textToSpeech"=>"Bad request"
+                        "textToSpeech"=>"response from host"
                          )
                     )
                 ],
                 ),
            
         ));
+
         
     }
-}
- 
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
- 
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["queryResult"]["action"])) {
-    processMessage($update);
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-   fwrite($myfile, $update["queryResult"]["action"]);
-    fclose($myfile);
-}else{
-     sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"Bad request"
-                         )
-                    )
-                ],
-                ),
-           
-        ));
+    
+    
+} else {
+    
+    echo "Method not allowed";
+    
 }
 
+?> 
 
-?>
+
+
+       
