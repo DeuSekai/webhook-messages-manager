5db1feb438737a3023cbff17f4ce0747fa6457ac
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-10 12:09:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,19 @@
-<?php
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if ($method == 'POST') {
-    
-    $requestBody = file_get_contents('php://input');
-    $json        = json_decode($requestBody);
-    
-    $text = $json->queryResult->intent->displayName;
-    
-    switch ($text) {
-        
-        case 'Saludo inicial inbox':
-            $week   = array(
-                "Domingo",
-                "Lunes",
-                "Martes",
-                "Miercoles",
-                "Jueves",
-                "Viernes",
-                "Sábado"
-            );
-            $day    = $week[date('w')];
-            //1 seconds delay
-            sleep(1);
-            $speech = "Hola, ¡feliz {$day}!, Podrías compartirnos tu nombre completo y de qué estado de la república nos contactas para brindarte un mejor servicio. 𝗧𝗲 𝗿𝗲𝗰𝗼𝗿𝗱𝗮𝗺𝗼𝘀 𝗾𝘂𝗲 𝗻𝘂𝗲𝘀𝘁𝗿𝗼𝘀 𝘁𝗿𝗮𝗺𝗶𝘁𝗲𝘀 𝘀𝗼𝗻 𝘁𝗼𝘁𝗮𝗹𝗺𝗲𝗻𝘁𝗲 𝗴𝗿𝗮𝘁𝘂𝗶𝘁𝗼𝘀 😊";
-            break;
-        
-        default:
-            //4 seconds delay
-            sleep(4);
-            $speech = NULL;
-            break;
-            
+if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
-    
-    $response              = new \stdClass();
-    $response->speech      = $speech;
-    $response->displayText = $speech;
-    $response->source      = "webhook";
-    
-    echo json_encode($response);
-    
-} else {
-    
-    echo "Method not allowed";
-    
 }
-
-?>
+ 
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+ 
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
