5477424564a807fad40ecbbe204e7f8973d9bb68
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-10 12:37:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 function processMessage($update) {
-    if($update["queryResult"]["action"] == "sayHello"){
+    if($update["queryResult"]["action"] == "input.welcome"){
+              $week = array(
+            "Domingo",
+            "Lunes",
+            "Martes",
+            "Miercoles",
+            "Jueves",
+            "Viernes",
+            "Sábado"
+        );
+        $day  = $week[date('w')];
+        //1 seconds delay
+        sleep(1);
         sendMessage(array(
             "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
+            "fulfillmentText"=>"Hola, ¡feliz {$day}!, Podrías compartirnos tu nombre completo y de qué estado de la república nos contactas para brindarte un mejor servicio. 𝗧𝗲 𝗿𝗲𝗰𝗼𝗿𝗱𝗮𝗺𝗼𝘀 𝗾𝘂𝗲 𝗻𝘂𝗲𝘀𝘁𝗿𝗼𝘀 𝘁𝗿𝗮𝗺𝗶𝘁𝗲𝘀 𝘀𝗼𝗻 𝘁𝗼𝘁𝗮𝗹𝗺𝗲𝗻𝘁𝗲 𝗴𝗿𝗮𝘁𝘂𝗶𝘁𝗼𝘀 😊",
             "payload" => array(
                 "items"=>[
                     array(
@@ -66,6 +78,8 @@ if (isset($update["queryResult"]["action"])) {
     processMessage($update);
     
 }else{
+            //4 seconds delay
+        sleep(4);
      sendMessage(array(
             "source" => $update["responseId"],
             "fulfillmentText"=>NULL,
